daad6dc604d15b4c10c9a1fb0d5a5c3832bf37e4
Chris-Vitalis <cvitalis@uw.edu>
2018-04-20 10:58:40 -0700

    Partial Work

--- exercise-4/exercise.R
+++ exercise-4/exercise.R
@@ -2,26 +2,21 @@
 
 # Install the `nycflights13` package. Load (`library()`) the package.
 # You'll also need to load `dplyr`
-
+library("dplyr")
+library("nycflights13")
 
 # The data frame `flights` should now be accessible to you.
 # Use functions to inspect it: how many rows and columns does it have?
 # What are the names of the columns?
 # Use `??flights` to search for documentation on the data set (for what the 
 # columns represent)
-
+nrow(flights)
 
 # Use `dplyr` to give the data frame a new column that is the amount of time
 # gained or lost while flying (that is: how much of the delay arriving occured
 # during flight, as opposed to before departing).
+ 
 
-
-# Use `dplyr` to sort your data frame in descending order by the column you just
-# created. Remember to save this as a variable (or in the same one!)
-
-
-# For practice, repeat the last 2 steps in a single statement using the pipe
-# operator. You can clear your environmental variables to "reset" the data frame
 
 
 # Make a histogram of the amount of time gained using the `hist()` function
--- exercise-5/exercise.R
+++ exercise-5/exercise.R
@@ -9,20 +9,27 @@ library(dplyr)
 # What was the average departure delay in each month?
 # Save this as a data frame `dep_delay_by_month`
 # Hint: you'll have to perform a grouping operation then summarizing your data
-
+dep_delay_by_month <- flights %>% 
+  group_by(month) %>% 
+  summarise(avg_delay = mean(dep_delay, na.rm = T))
 
 # Which month had the greatest average departure delay?
+dep_delay_by_month <- arrange(dep_delay_by_month, avg_delay)
 
+max <- dep_delay_by_month %>% 
+  select(max(avg_delay))
 
 # If your above data frame contains just two columns (e.g., "month", and "delay" in that order), you can create
 # a scatterplot by passing that data frame to the 'plot()' function
-
+plot(dep_delay_by_month, col2rgb(green))
 
 # To which destinations were the average arrival delays the highest?
 # Hint: you'll have to perform a grouping operation then summarize your data
 # You can use the `head()` function to view just the first few rows
-
-
+dest_most_delay <- flights %>% 
+  group_by(dest) %>% 
+  summarise(avg_delay_boi = mean(arr_delay, na.rm = T)) %>% 
+arrange(-avg_delay_boi)
 # You can look up these airports in the `airports` data frame!
 
 
